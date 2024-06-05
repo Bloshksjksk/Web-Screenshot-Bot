cb94a2d496ccc409e91b7df90538c5b937a7dba1
Bloshksjksk <muscular.colossal.policeman@ghostlymail.com>
2024-06-05 09:51:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,8 +25,8 @@ def init_request_timeout() -> int:
 
 class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN","5879463342:AAHyGLH_1VcBhjljJmLjaGzUD8upxkOUu5o")
-    API_ID = int(os.environ("API_ID","4682685"))
-    API_HASH = os.environ("API_HASH","3eba5d471162181b8a3f7f5c0a23c307")
+    API_ID = int(os.environ.get("API_ID","4682685"))
+    API_HASH = os.environ.get("API_HASH","3eba5d471162181b8a3f7f5c0a23c307")
     EXEC_PATH = os.environ.get("GOOGLE_CHROME_SHIM", None)
     # OPTIONAL
     LOG_GROUP = init_log()
