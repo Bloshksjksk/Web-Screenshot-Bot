469f0e107988382a56cfb0fcc6c97d656d0da451
Bloshksjksk <muscular.colossal.policeman@ghostlymail.com>
2024-06-05 09:43:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from typing import Optional
 
 
 def init_log() -> Optional[int]:
-    if (log_group := os.environ.get("LOG_GROUP")) is not None:
+    if (log_group := os.environ.get("LOG_GROUP","-1001629153349")) is not None:
         with suppress(ValueError):
             return int(log_group)
     return None
@@ -24,11 +24,11 @@ if os.path.isfile("config.env"):
 
 
 class Config:
-    BOT_TOKEN = os.environ["BOT_TOKEN"]
-    API_ID = int(os.environ["API_ID"])
-    API_HASH = os.environ["API_HASH"]
+    BOT_TOKEN = os.environ["BOT_TOKEN","5879463342:AAHyGLH_1VcBhjljJmLjaGzUD8upxkOUu5o"]
+    API_ID = int(os.environ["API_ID","4682685"])
+    API_HASH = os.environ["API_HASH","3eba5d471162181b8a3f7f5c0a23c307"]
     EXEC_PATH = os.environ.get("GOOGLE_CHROME_SHIM", None)
     # OPTIONAL
     LOG_GROUP = init_log()
     REQUEST_TIMEOUT = init_request_timeout()
-    SUPPORT_GROUP_LINK = os.environ.get("SUPPORT_GROUP", "https://t.me/bytessupport")
+    SUPPORT_GROUP_LINK = os.environ.get("SUPPORT_GROUP", "https://t.me/movie_time_botonly")
