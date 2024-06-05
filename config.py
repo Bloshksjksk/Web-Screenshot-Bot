126dc99c5da3bbdc9fff00e8f02dc93e1cc58da5
Bloshksjksk <muscular.colossal.policeman@ghostlymail.com>
2024-06-05 10:42:58 +0530

    🏆TRUMBOTS NEW FEATURES🏆

--- config.py
+++ config.py
@@ -1,34 +1,10 @@
 import os
-from contextlib import suppress
-from typing import Optional
 
-
-
-
-def init_log() -> Optional[int]:
-    if (log_group := os.environ.get("LOG_GROUP","-1001629153349")) is not None:
-        with suppress(ValueError):
-            return int(log_group)
-    return None
-
-
-def init_request_timeout() -> int:
-    request_timeout = os.environ.get("REQUEST_TIMEOUT", "30")
-    with suppress(ValueError):
-        return int(request_timeout)
-    return 30
-
-
-#if os.path.isfile("config.env"):
- #   load_dotenv("config.env")
-
-
-class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN","5879463342:AAHyGLH_1VcBhjljJmLjaGzUD8upxkOUu5o")
-    API_ID = int(os.environ.get("API_ID","4682685"))
-    API_HASH = os.environ.get("API_HASH","3eba5d471162181b8a3f7f5c0a23c307")
-    EXEC_PATH = os.environ.get("GOOGLE_CHROME_SHIM", None)
-    # OPTIONAL
-    LOG_GROUP = init_log()
-    REQUEST_TIMEOUT = init_request_timeout()
-    SUPPORT_GROUP_LINK = os.environ.get("SUPPORT_GROUP", "https://t.me/movie_time_botonly")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5484278199:AAFyPN7RbJQC4usLJQ_WtBuE0jtqiInIGpA")
+API_ID = int(os.environ.get("API_ID", "4682685"))
+API_HASH = os.environ.get("API_HASH", "3eba5d471162181b8a3f7f5c0a23c307")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002182387390"))
+AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "945284066").split())
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://misoc51233:ZlP391e4m0IIS85S@cluster0.8xs2zsl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "BroadcastBot")
+BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
