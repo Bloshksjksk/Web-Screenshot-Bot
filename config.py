02234300e9db4397d89a1f83f6beae9ca66ba942
Bloshksjksk <muscular.colossal.policeman@ghostlymail.com>
2024-06-05 10:52:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 from contextlib import suppress
 from typing import Optional
 
-from dotenv import load_dotenv
+
 
 
 def init_log() -> Optional[int]:
-    if (log_group := os.environ.get("LOG_GROUP")) is not None:
+    if (log_group := os.environ.get("LOG_GROUP","-1001629153349")) is not None:
         with suppress(ValueError):
             return int(log_group)
     return None
@@ -19,16 +19,15 @@ def init_request_timeout() -> int:
     return 30
 
 
-if os.path.isfile("config.env"):
-    load_dotenv("config.env")
+
 
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN","")
+    API_ID = int(os.environ.get("API_ID","4682685"))
+    API_HASH = os.environ.get("API_HASH","3eba5d471162181b8a3f7f5c0a23c307")
     EXEC_PATH = os.environ.get("GOOGLE_CHROME_SHIM", None)
     # OPTIONAL
     LOG_GROUP = init_log()
     REQUEST_TIMEOUT = init_request_timeout()
-    SUPPORT_GROUP_LINK = os.environ.get("SUPPORT_GROUP", "https://t.me/movie_time")
+    SUPPORT_GROUP_LINK = os.environ.get("SUPPORT_GROUP", "https://t.me/movie_time_botonly")
